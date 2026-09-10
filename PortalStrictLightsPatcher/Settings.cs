60bfafcff74d252e7e90a7cb9c0a249f107b40aa
Petri Hoskonen <route.electrique@gmail.com>
2026-09-10 10:24:39 +0300

    main v1 test

---
+++ PortalStrictLightsPatcher/Settings.cs
@@ -0,0 +1,8 @@
+namespace PortalStrictLightsPatcher;
+
+public class Settings
+{
+    public bool IncludeSpotlights { get; set; } = false;
+
+    public bool IncludeMagicLights { get; set; } = false;
+}
